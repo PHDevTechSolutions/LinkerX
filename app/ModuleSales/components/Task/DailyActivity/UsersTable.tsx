7153e57f067bbce0e66e0c6d0ece933c94b55b38
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-03-08 16:44:27 +0800

    Leroux Y Xchire

--- app/ModuleSales/components/Task/DailyActivity/UsersTable.tsx
+++ app/ModuleSales/components/Task/DailyActivity/UsersTable.tsx
@@ -213,14 +213,14 @@ const UsersCard: React.FC<UsersCardProps> = ({ posts, handleEdit, handleStatusUp
                                                         <BsThreeDotsVertical size={16} />
                                                     </button>
                                                     {/* Dropdown Menu */}
-                                                    <div id={`menu-${user.id}`} className={`absolute right-0 mt-2 w-32 bg-white shadow-md p-2 rounded-md text-xs ${openMenu === user.id ? 'block' : 'hidden'}`}>
+                                                    <div id={`menu-${user.id}`} className={`absolute right-0 mt-2 w-32 bg-white shadow-md p-2 rounded-md text-xs ${openMenu === user.id ? 'block' : 'hidden'} z-50`}>
                                                         <ul>
                                                             <li className="p-2 cursor-pointer hover:bg-gray-100 flex items-center gap-1" onClick={() => handleEdit(user)}><CiEdit /> Edit Details</li>
                                                             <li className="p-2 cursor-pointer hover:bg-gray-100 flex items-center gap-1" onClick={() => toggleStatusMenu(user.id)}><BsRecycle />Change Status</li>
 
                                                             {/* Status Change Menu */}
                                                             {statusMenuVisible[user.id] && (
-                                                                <div className="absolute left-[-10rem] top-0 bg-white shadow-lg rounded-lg border w-40 z-10 mr-2 text-xs">
+                                                                <div className="absolute left-[-10rem] bottom-full mb-2 bg-white shadow-lg rounded-lg border w-40 z-50 mr-2 text-xs">
                                                                     {Object.keys(STATUS_COLORS).map((activitystatus) => (
                                                                         <button key={activitystatus} onClick={() => updateStatus(user.id, activitystatus)} className="w-full flex items-center gap-2 text-left px-4 py-2 hover:bg-gray-100">
                                                                             <span className={`w-3 h-3 rounded-full border border-black ${STATUS_COLORS[activitystatus].split(" ")[0]}`}></span>
@@ -229,12 +229,14 @@ const UsersCard: React.FC<UsersCardProps> = ({ posts, handleEdit, handleStatusUp
                                                                     ))}
                                                                 </div>
                                                             )}
+
                                                             <li className="p-2 cursor-pointer hover:bg-gray-100 flex items-center gap-1" onClick={() => handlePin(user.id)}>
                                                                 <CiMapPin /> {pinnedUsers.has(user.id) ? "Unpin" : "Pin"}
                                                             </li>
                                                             <li className="p-2 cursor-pointer hover:bg-gray-100 flex items-center gap-1" onClick={() => handleDelete(user.id)}><CiTrash />Delete</li>
                                                         </ul>
                                                     </div>
+
                                                 </div>
                                             </div>
                                         )}
