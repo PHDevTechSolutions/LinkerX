f91f116299c51b29966bb5640d5edd644d6d3b14
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-03-15 09:49:55 +0800

    Leroux Y Xchire

--- app/ModuleCSR/components/Sidebar/Sidebar.tsx
+++ app/ModuleCSR/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,10 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { BsFillTelephoneOutboundFill } from "react-icons/bs";
-import { FaBuildingUser, FaHeadphonesSimple, FaUsersGear, FaPlus, FaMinus } from "react-icons/fa6";
-import { MdOutlineSpaceDashboard } from "react-icons/md";
-import { IoMdSettings, IoIosAnalytics, IoIosHelpCircleOutline  } from "react-icons/io";
+import { FaBuildingUser, FaPlus, FaMinus } from "react-icons/fa6";
 import { FaRegCircle } from "react-icons/fa";
-import { LuLayoutDashboard } from "react-icons/lu";
 import TaskflowIcon from './TaskflowIcon';
+import { CiGrid42, CiInboxIn, CiWavePulse1, CiUser, CiCircleQuestion, CiSettings } from "react-icons/ci";
 
 import Link from 'next/link';
 import { useRouter } from "next/navigation";
@@ -16,7 +13,7 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
   const [collapsed, setCollapsed] = useState(false);
   const [openSections, setOpenSections] = useState<Record<string, boolean>>({});
   const [userId, setUserId] = useState<string | null>(null);
-  const [userDetails, setUserDetails] = useState({ReferenceID: "", Firstname: "", Lastname: "", Location: "", Role: "",});
+  const [userDetails, setUserDetails] = useState({ ReferenceID: "", Firstname: "", Lastname: "", Location: "", Role: "", });
   const router = useRouter();
 
   useEffect(() => {
@@ -62,7 +59,7 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
   const menuItems = [
     {
       title: 'CSR Inquiries',
-      icon: BsFillTelephoneOutboundFill,
+      icon: CiInboxIn,
       subItems: [
         { title: 'Tickets', href: `/ModuleCSR/CSR/Monitoring/Activities${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
         userDetails.ReferenceID === 'LR-CSR-849432'
@@ -72,14 +69,14 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
     },
     {
       title: 'Customer Database',
-      icon: FaBuildingUser ,
+      icon: FaBuildingUser,
       subItems: [
         { title: 'List of Accounts', href: `/ModuleCSR/CSR/Accounts/AccountList${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
       ],
     },
     {
       title: 'Reports',
-      icon: IoIosAnalytics ,
+      icon: CiWavePulse1,
       subItems: [
         { title: 'Daily CSR Transaction', href: `/ModuleCSR/CSR/Reports/DailyTransaction${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
         { title: 'SKU Listing', href: `/ModuleCSR/CSR/Reports/SKUListing${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
@@ -89,7 +86,7 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
     },
     {
       title: 'Taskflow',
-      icon: TaskflowIcon ,
+      icon: TaskflowIcon,
       subItems: [
         { title: 'Outbound Calls', href: `/ModuleCSR/CSR/Calls/OutboundCall${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
         { title: 'Database', href: `/ModuleCSR/CSR/Calls/Database${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
@@ -97,28 +94,28 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
     },
     {
       title: 'User Creation',
-      icon: FaUsersGear,
+      icon: CiUser,
       subItems: [
         { title: 'List of Users', href: `/ModuleCSR/CSR/Users/ListofUser${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
       ],
     },
     {
       title: 'Eco Help',
-      icon: IoIosHelpCircleOutline,
+      icon: CiCircleQuestion,
       subItems: [
         { title: 'CSR Faqs', href: `/ModuleCSR/CSR/Help${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
       ],
     },
     {
       title: 'Settings',
-      icon: IoMdSettings,
+      icon: CiSettings,
       subItems: [
         { title: 'Update Profile', href: `/ModuleCSR/CSR/Setting/Profile${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
         { title: 'Developers', href: `/ModuleCSR/CSR/Setting/Developers${userId ? `?id=${encodeURIComponent(userId)}` : ''}` },
       ],
     },
   ];
-  
+
 
   // Filter menu items based on the user's role
   const filteredMenuItems = menuItems.filter((item) => {
@@ -135,7 +132,7 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
       {/* Logo Section */}
       <div className="flex items-center justify-between p-4">
         <div className="flex items-center">
-          <img src="/ecodesk.png" alt="Logo" className="h-8 mr-2 bg-gray-50 shadow-lg rounded-full"/>
+          <img src="/ecodesk.png" alt="Logo" className="h-8 mr-2 bg-gray-50 shadow-lg rounded-full" />
           <Link href={`/ModuleCSR/CSR/Dashboard${userId ? `?id=${encodeURIComponent(userId)}` : ''}`}>
             <h1 className={`text-md font-bold transition-opacity ${collapsed ? "opacity-0" : "opacity-100"}`}>
               ECODESK
@@ -146,65 +143,65 @@ const Sidebar: React.FC<{ isOpen: boolean, onClose: () => void }> = ({ isOpen, o
 
       {/* User Details Section */}
       {!collapsed && (
-          <div className="p-8 text-xs text-left border-b">
-            <p className="font-bold uppercase">
+        <div className="p-8 text-xs text-left border-b">
+          <p className="font-bold uppercase">
             {userDetails.Firstname}, {userDetails.Lastname}
-            </p>
-            <p>{userDetails.ReferenceID}</p>
-            <p>( {userDetails.Role} )</p>
-          </div>
-        )}
-      
+          </p>
+          <p>{userDetails.ReferenceID}</p>
+          <p>( {userDetails.Role} )</p>
+        </div>
+      )}
+
       {/* Menu Section */}
       <div className="flex flex-col items-center rounded-md flex-grow overflow-y-auto text-xs p-2">
         <div className="w-full">
-          <Link href={`/ModuleCSR/CSR/Dashboard/${userId ? `?id=${encodeURIComponent(userId)}` : ''}`} className="flex items-center w-full p-4 bg-gray-50 rounded text-gray-900 transition-all"><MdOutlineSpaceDashboard size={22} className="mr-1"/>Dashboard</Link>
+          <Link href={`/ModuleCSR/CSR/Dashboard/${userId ? `?id=${encodeURIComponent(userId)}` : ''}`} className="flex items-center w-full p-4 bg-gray-50 rounded text-gray-900 transition-all">
+          <CiGrid42 size={20} className="mr-1" />Dashboard</Link>
         </div>
-          {filteredMenuItems.map((item, index) => (
-            <div key={index} className="w-full">
-              <button
-                onClick={() => handleToggle(item.title)}
-                className={`flex items-center w-full p-4 mt-1 transition-all duration-300 ease-in-out 
+        {filteredMenuItems.map((item, index) => (
+          <div key={index} className="w-full">
+            <button
+              onClick={() => handleToggle(item.title)}
+              className={`flex items-center w-full p-4 mt-1 transition-all duration-300 ease-in-out 
                   hover:bg-gray-50 hover:rounded-md hover:text-gray-900 hover:shadow-md 
                   active:scale-95 ${collapsed ? "justify-center" : ""}`}
-                
-              >
-                <item.icon size={18} />
-                {!collapsed && <span className="ml-2">{item.title}</span>}
-                {!collapsed && (
-                  <span className="ml-auto">
-                    {openSections[item.title] ? <FaMinus size={10} /> : <FaPlus size={10} />}
-                  </span>
-                )}
-              </button>
-              <div
-                className={`overflow-hidden transition-all duration-300 ease-in-out ${
-                  openSections[item.title] ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
-                }`}
-              >
-                {openSections[item.title] && !collapsed && (
-  <div> {/* Added margin-left for submenu spacing */}
-    {Array.isArray(item.subItems) &&
-      item.subItems.map((subItem, subIndex) =>
-        subItem && typeof subItem === "object" && "href" in subItem ? (
-          <Link
-            key={subIndex}
-            href={subItem.href}
-            prefetch={true}
-            className="flex w-full items-center p-4 bg-gray-50 text-gray-900 border-l-2 border-transparent transition-all duration-300 ease-in-out hover:bg-yellow-100 hover:shadow-md"
-          >
-            <FaRegCircle size={10} className="mr-2 ml-2" />
-            {subItem.title}
-          </Link>
-        ) : null
-      )}
-  </div>
-)}
 
-              </div>
+            >
+              <item.icon size={18} />
+              {!collapsed && <span className="ml-2">{item.title}</span>}
+              {!collapsed && (
+                <span className="ml-auto">
+                  {openSections[item.title] ? <FaMinus size={10} /> : <FaPlus size={10} />}
+                </span>
+              )}
+            </button>
+            <div
+              className={`overflow-hidden transition-all duration-300 ease-in-out ${openSections[item.title] ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+                }`}
+            >
+              {openSections[item.title] && !collapsed && (
+                <div> {/* Added margin-left for submenu spacing */}
+                  {Array.isArray(item.subItems) &&
+                    item.subItems.map((subItem, subIndex) =>
+                      subItem && typeof subItem === "object" && "href" in subItem ? (
+                        <Link
+                          key={subIndex}
+                          href={subItem.href}
+                          prefetch={true}
+                          className="flex w-full items-center p-4 bg-gray-50 text-gray-900 border-l-2 border-transparent transition-all duration-300 ease-in-out hover:bg-yellow-100 hover:shadow-md"
+                        >
+                          <FaRegCircle size={10} className="mr-2 ml-2" />
+                          {subItem.title}
+                        </Link>
+                      ) : null
+                    )}
+                </div>
+              )}
+
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
