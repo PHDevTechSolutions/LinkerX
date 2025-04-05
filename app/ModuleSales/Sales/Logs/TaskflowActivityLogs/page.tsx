d664fb662df096a4fa31172cd13f2dfe892d1809
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-04-05 10:46:06 +0800

    Leroux Y Xchire

--- app/ModuleSales/Sales/Logs/TaskflowActivityLogs/page.tsx
+++ app/ModuleSales/Sales/Logs/TaskflowActivityLogs/page.tsx
@@ -439,6 +439,7 @@ const ListofUser: React.FC = () => {
                                                         <option value="1000000">1,000,000</option>
                                                         <option value="950000">950,000</option>
                                                         <option value="700000">700,000</option>
+                                                        <option value="500000">500,000</option>
                                                         <option value="300000">300,000</option>
                                                         <option value="0">0</option>
                                                     </select>
