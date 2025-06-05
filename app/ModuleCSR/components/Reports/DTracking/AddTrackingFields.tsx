2b204b1d32bfd8c4242ab86a70eb5c3ae5e2ac1a
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2025-06-05 14:26:59 +0800

    Update AddTrackingFields.tsx

--- app/ModuleCSR/components/Reports/DTracking/AddTrackingFields.tsx
+++ app/ModuleCSR/components/Reports/DTracking/AddTrackingFields.tsx
@@ -268,6 +268,7 @@ const AddTrackingFields: React.FC<ReceivedFieldsProps> = ({
                     <select id="Department" value={Department} onChange={(e) => setDepartment(e.target.value)} className="w-full px-3 py-2 border rounded text-xs bg-gray-50">
                         <option value="">Select Department</option>
                         <option value="Accounting">Accounting</option>
+                        <option value="E-Commerce">E-Commerce</option>
                         <option value="Engineering">Engineering</option>
                         <option value="Human Resources">Human Resources</option>
                         <option value="Marketing">Marketing</option>
