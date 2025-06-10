12a4888be3eebc8b82139bfaa32b9f66c74a8d24
PHDevTechSolutions <phdevtechsolutions@gmail.com>
2025-06-10 11:57:27 +0800

    Leroux Y Xchire

--- app/ModuleSales/components/Task/ScheduledTask/MainCardTable.tsx
+++ app/ModuleSales/components/Task/ScheduledTask/MainCardTable.tsx
@@ -47,7 +47,7 @@ interface MainCardTableProps {
 
 const MainCardTable: React.FC<MainCardTableProps> = ({ posts, userDetails, fetchAccount }) => {
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [itemsPerPage, setItemsPerPage] = useState(50);
     const [view, setView] = useState<"table" | "grid" | "card">("table");
     const [showForm, setShowForm] = useState(false);
     const [editUser, setEditUser] = useState<Post | null>(null);
